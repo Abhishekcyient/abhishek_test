3c66de91f18902b4b4f11ff490f95f1493fc7b2d
Abhishekcyient <111974171+Abhishekcyient@users.noreply.github.com>
2022-08-25 18:00:03 +0530

    program for ADD,SUB,DIV

---
+++ program_1/add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int add, sub, mul;
+  float div;
+ 
+  //addition
+  add = 5 + 2;
+ 
+  //subtraction
+  sub = 5 - 2;
+ 
+  //division
+  div = 5 / (float)2;
+ 
+  printf("Add: %d, Sub: %d, Div: %.2f", add,sub,div);
+  return 0;
+}
